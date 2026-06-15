561c21cce82ac6be3146cdf57927c110f94a0447
Darwin Bracho <darwindavidbracho@gmail.com>
2026-06-15 14:22:51 -0500

    Prevent cached dashboard after logout

--- backend/apps/dashboard/views.py
+++ backend/apps/dashboard/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 
+@never_cache
 @ensure_csrf_cookie
 def login_view(request):
     return render(request, 'login.html')
 
 
+@never_cache
 @ensure_csrf_cookie
 def dashboard_view(request):
     return render(request, 'dashboard.html')
